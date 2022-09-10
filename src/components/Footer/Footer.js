1ffeaacc71b3dc1a98b2195e9939b4907c026e2e
MarkVed17 <vedantlahane17@gmail.com>
2022-09-10 22:54:43 +0530

    feat: created `Footer` component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,47 @@
+import "./Footer.css";
+import img from "../../assets/images/logo/white.svg";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__main">
+        <div className="footer__logo">
+          <img src={img} alt="dukaan" />
+        </div>
+        <div className="footer__actions">
+          <div className="footer__group">
+            <a href="https://dukaan-dummy.netlify.app/">Contact</a>
+            <a href="https://dukaan-dummy.netlify.app/">FAQ's</a>
+          </div>
+          <div className="footer__group">
+            <a href="https://dukaan-dummy.netlify.app/">Tutorials</a>
+            <a href="https://dukaan-dummy.netlify.app/">Blog</a>
+          </div>
+          <div className="footer__group">
+            <a href="https://dukaan-dummy.netlify.app/">Privacy</a>
+            <a href="https://dukaan-dummy.netlify.app/">Banned Items</a>
+          </div>
+          <div className="footer__group">
+            <a href="https://dukaan-dummy.netlify.app/">About</a>
+            <div className="footer__notifications">
+              <a href="https://dukaan-dummy.netlify.app/">Jobs</a>
+              <span className="footer__badge">3</span>
+            </div>
+          </div>
+          <div className="footer__group">
+            <a href="https://dukaan-dummy.netlify.app/">Facebook</a>
+            <a href="https://dukaan-dummy.netlify.app/">Twitter</a>
+            <a href="https://dukaan-dummy.netlify.app/">LinkedIn</a>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="footer__copyright">
+        <span>Dukaan 2020, All rights reserved.</span>
+        <span>Made in India</span>
+      </div>
+    </footer>
+  );
+};
+
+export { Footer };
