98ca6f6b695b0da613a912ea271575c86e40397e
MarkVed17 <vedantlahane17@gmail.com>
2022-09-11 03:52:19 +0530

    added `Slogan Maker` component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,8 +1,10 @@
 import { StepsBanner } from "../Banners/StepsBanner/StepsBanner";
+import { SloganMaker } from "../SloganMaker/SloganMaker";
 
 const Main = () => {
   return (
     <>
+      <SloganMaker />
       <StepsBanner />
     </>
   );
