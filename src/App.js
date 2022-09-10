9b397ef44e007375f1277234da4ba848d249d739
MarkVed17 <vedantlahane17@gmail.com>
2022-09-11 00:03:02 +0530

    added `Main` component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import { Header, Footer } from "./components";
+import { Header, Footer, Main } from "./components";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
       <Footer />
     </div>
   );
