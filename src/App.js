16778f74581934c71813a7a9a2b92bd8cc097f55
MarkVed17 <vedantlahane17@gmail.com>
2022-09-11 03:49:03 +0530

    dependency: added `react-hot-toast`

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import { Header, Footer, Main } from "./components";
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div className="App">
+      <Toaster />
       <Header />
       <Main />
       <Footer />
