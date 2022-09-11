e9e5986dd6ff27d167d3f3ca0643a922926f3eec
MarkVed17 <vedantlahane17@gmail.com>
2022-09-12 04:48:45 +0530

    feat: `Pagination` for slogans

---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,50 @@
+import "./Pagination.css";
+
+const Pagination = ({
+  currentPage,
+  slogansPerPage,
+  totalSlogans,
+  paginate,
+}) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalSlogans / slogansPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  console.log(pageNumbers);
+
+  return (
+    <nav className="pagination-container">
+      <ul className="pagination">
+        {pageNumbers.map((number) => (
+          <li
+            key={number}
+            className={
+              number === currentPage ? "active-page-item" : "page-item"
+            }
+          >
+            <button
+              onClick={() => paginate(number)}
+              className={
+                number === currentPage ? "active-page-link" : "page-link"
+              }
+            >
+              {number}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <div className="pagination-next">
+        <button className="pagination-next-btn">Next</button>
+        <img
+          src="/assets/images/icons/next.svg"
+          alt=">"
+          className="pagination-next-icon"
+        />
+      </div>
+    </nav>
+  );
+};
+
+export { Pagination };
--- src/components/SloganMaker/SloganMaker.js
+++ src/components/SloganMaker/SloganMaker.js
@@ -1,9 +1,22 @@
 import "./SloganMaker.css";
 import toast from "react-hot-toast";
 import data from "../../data/data.json";
+import { useState } from "react";
+import { Pagination } from "../Pagination/Pagination";
 
 const SloganMaker = () => {
   const { slogans } = data;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [slogansPerPage] = useState(18);
+
+  console.log(slogans.length);
+
+  // Get current slogans
+  const indexOfLastSlogan = currentPage * slogansPerPage;
+  const indexOfFirstSlogan = indexOfLastSlogan - slogansPerPage;
+  const currentSlogans = slogans.slice(indexOfFirstSlogan, indexOfLastSlogan);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   const copyToClipboard = (text) => {
     navigator.clipboard.writeText(text);
@@ -44,7 +57,7 @@ const SloganMaker = () => {
           <button className="slogan-maker__download-btn">Download all</button>
         </div>
         <div className="slogan-maker__slogans">
-          {slogans.map(({ id, text }) => (
+          {currentSlogans.map(({ id, text }) => (
             <div
               key={id}
               className="slogan-maker__slogan"
@@ -55,6 +68,12 @@ const SloganMaker = () => {
           ))}
         </div>
       </div>
+      <Pagination
+        currentPage={currentPage}
+        slogansPerPage={slogansPerPage}
+        totalSlogans={slogans.length}
+        paginate={paginate}
+      />
     </section>
   );
 };
