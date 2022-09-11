f5e40993656ca7eedf90d4e00857106a05cdc860
MarkVed17 <vedantlahane17@gmail.com>
2022-09-11 13:28:05 +0530

    added `Suggestions Banner` component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,12 +1,15 @@
 import { StepsBanner } from "../Banners/StepsBanner/StepsBanner";
+import { SuggestionsBanner } from "../Banners/SuggestionsBanner/SuggestionsBanner";
 import { SloganMaker } from "../SloganMaker/SloganMaker";
+import "./Main.css";
 
 const Main = () => {
   return (
-    <>
+    <div className="main-content">
       <SloganMaker />
       <StepsBanner />
-    </>
+      <SuggestionsBanner />
+    </div>
   );
 };
 
