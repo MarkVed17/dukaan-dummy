86bffc1f22ac6a1853d2855501d5cdb67a47d7d0
MarkVed17 <vedantlahane17@gmail.com>
2022-09-12 04:49:25 +0530

    barrel exports

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { Main } from "./Main/Main";
 export { StepsBanner } from "./Banners/StepsBanner/StepsBanner";
 export { SuggestionsBanner } from "./Banners/SuggestionsBanner/SuggestionsBanner";
 export { SloganMaker } from "./SloganMaker/SloganMaker";
+export { Pagination } from "./Pagination/Pagination";
