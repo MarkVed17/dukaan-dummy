e573652ae9e517ee8f0fff9f01b94779d78f078d
MarkVed17 <vedantlahane17@gmail.com>
2022-09-12 13:12:11 +0530

    added banner title

--- src/components/Banners/SuggestionsBanner/SuggestionsBanner.js
+++ src/components/Banners/SuggestionsBanner/SuggestionsBanner.js
@@ -6,19 +6,24 @@ const SuggestionsBanner = () => {
 
   return (
     <section className="suggestions__banner">
-      {suggestedProducts.map(({ id, imgPath, title, subtitle }) => (
-        <div key={id} className="suggestions-banner__suggestion">
-          <div className="suggestion-banner__suggestion-media">
-            <img
-              src={imgPath}
-              alt="privacy policy"
-              className="suggestions-banner__suggestion-img"
-            />
-            <p className="suggestions-banner__suggestion-title">{title}</p>
+      <h1 className="suggestions__banner-title">Try our other free products</h1>
+      <div className="suggestions__banner-products">
+        {suggestedProducts.map(({ id, imgPath, title, subtitle }) => (
+          <div key={id} className="suggestions-banner__suggestion">
+            <div className="suggestion-banner__suggestion-media">
+              <img
+                src={imgPath}
+                alt="privacy policy"
+                className="suggestions-banner__suggestion-img"
+              />
+              <p className="suggestions-banner__suggestion-title">{title}</p>
+            </div>
+            <p className="suggestions-banner__suggestion-subtitle">
+              {subtitle}
+            </p>
           </div>
-          <p className="suggestions-banner__suggestion-subtitle">{subtitle}</p>
-        </div>
-      ))}
+        ))}
+      </div>
     </section>
   );
 };
